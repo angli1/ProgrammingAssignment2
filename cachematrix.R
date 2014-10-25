f3f43b04d78218f706fe95a4f44470e54545ab60
angli1 <ang.li1@gmail.com>
2014-10-25 07:06:20 -0500

    edit file, more readable in github

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,11 @@
 ## the scoping rules of R language, set, compute, and retrieve the inverse of a given matrix. 
 ## 
 ## examples of usage:
-## matrix_test <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))  ### the given matrix is a 2x2 square matrix 
-## matrix_test$get()     							## check the given matrix 
-## cacheSove(matrix_test) 							## get the inverse of the given matrix. 																					
-## matrix_test$set(matrix(rnorm(16), 4, 4)))    	## set a new 4x4 square, 
-## cacheSolve(matrix_test) 							## compute the inverse of new 4x4 matrix 
+## matrix_test <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))   ## the given matrix is a 2x2 square matrix 
+## matrix_test$get()     				## check the given matrix 
+## cacheSove(matrix_test) 				## get the inverse of the given matrix																				
+## matrix_test$set(matrix(rnorm(16), 4, 4)))    	## set a new 4x4 square matrix 
+							## cacheSolve(matrix_test) 							## compute the inverse of new 3x4 matrix 
    
 
 ## The following function  "makeCacheMatrix",  creates a special "matrix" object, 
@@ -20,18 +20,18 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-	inv <- NULL										## "inv" is the variable used to hold the inverse of the matrix
+	inv <- NULL					## "inv" is the variable used to hold the inverse of the matrix
 	set <- function(y) {
-			x <<- y									## the super-assignment operator, assigns value to x in the 
-													## containing environment(i.e., global variable here) 
-			inv <<- NULL							## similarly,  inv here is a global variable
+		x <<- y					## the super-assignment operator, assigns value to x in the 
+							## containing environment(i.e., global variable here) 
+		inv <<- NULL				## similarly,  inv here is a global variable
 	}
 	get <- function() x								## retrieve the given matrix
-	setinverse <- function(solve) inv <<- solve		## "solve" is a function used to compute the inverse of a given matrix
-	getinverse <- function() inv					## retrieve the value of "inv", which is the inverse of a given matrix 
+	setinverse <- function(solve) inv <<- solve	## "solve" is a function used to compute the inverse of a given matrix
+	getinverse <- function() inv			## retrieve the value of "inv", which is the inverse of a given matrix 
 	list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)  
+            setinverse = setinverse,
+            getinverse = getinverse)  
   
 }
 
@@ -44,8 +44,8 @@ cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
 	inv <- x$getinverse()
 	if(!is.null(inv)) {							## if variable inv is not null, then return the cached value
-			message("getting cached data")
-			return(inv)
+		message("getting cached data")
+		return(inv)
 	}
 	data <- x$get()								## assign the given matrix to variable "data"
 	inv <- solve(data, ...)						## compute the inverse of the matrix using "solve" function
